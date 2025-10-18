c720802b73dd4858b10a37ed9351a9f37f46121d
JOSE TORRES <joseandrestorres0508@gmail.com>
2025-10-18 17:45:54 -0500

    chore: agregar dependéncia Validation para validar los DTO de petición.

--- src/main/java/com/app/account/dto/request/AccountRequestDTO.java
+++ src/main/java/com/app/account/dto/request/AccountRequestDTO.java
@@ -1,6 +1,10 @@
 package com.app.account.dto.request;
 
+import java.math.BigDecimal;
+
 public record AccountRequestDTO(
-    String numeroCuenta
+    String numeroCuenta,
+    String dueno,
+    BigDecimal balanceActual
 ) {
 }
